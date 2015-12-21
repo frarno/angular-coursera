31186d61c44e99751e32a656307c4fc4d1e4c9b2
Arnaud MONTEILHET <fr.92.arno@gmail.com>
2015-12-21 06:57:34 +0100

    add angular scope

--- conFusion/gulpfile.js
+++ conFusion/gulpfile.js
@@ -12,7 +12,8 @@ var gulp = require('gulp'),
     changed = require('gulp-changed'),
     rev = require('gulp-rev'),
     browserSync = require('browser-sync'),
-    del = require('del');
+    del = require('del'),
+    ngannotate = require('gulp-ng-annotate');
 
 gulp.task('jshint', function() {
   return gulp.src('app/scripts/**/*.js')
@@ -35,10 +36,12 @@ gulp.task('usemin',['jshint'], function () {
   return gulp.src('./app/menu.html')
       .pipe(usemin({
         css:[minifycss(),rev()],
-        js: [uglify(),rev()]
+        js: [ngannotate(),uglify(),rev()]
       }))
       .pipe(gulp.dest('dist/'));
 });
+
+
 
 // Images
 gulp.task('imagemin', function() {
