052cb513cc7df3936022048bf8fb023c2f4ad900
Arnaud MONTEILHET <fr.92.arno@gmail.com>
2015-12-23 14:34:03 +0100

    updage week 2 assignement add comment form

--- conFusion/app/scripts/app.js
+++ conFusion/app/scripts/app.js
@@ -151,23 +151,14 @@ angular.module('confusionApp', [])
             $scope.dish = dish;
             
         }])
-
         .controller('DishCommentController', ['$scope', function($scope) {
-            
-            //Step 1: Create a JavaScript object to hold the comment from the form
-            
+            $scope.comment = {author:"", rating:5, comment:"", date:""};
+        
             $scope.submitComment = function () {
-                
-                //Step 2: This is how you record the date
-                "The date property of your JavaScript object holding the comment" = new Date().toISOString();
-                
-                // Step 3: Push your comment into the dish's comment array
-                $scope.dish.comments.push("Your JavaScript Object holding the comment");
-                
-                //Step 4: reset your form to pristine
-                
-                //Step 5: reset your JavaScript object that holds your comment
+                    $scope.comment.date = new Date().toISOString();
+                    $scope.dish.comments.push($scope.comment)
+                    $scope.commentForm.$setPristine();
+                    $scope.comment = {author:"", rating:5, comment:"", date:""};
             }
-        }])
-
+        }])          
 ;
