463632f9fdb40505ad021b7a534c9720c3fa896b
Arnaud MONTEILHET <fr.92.arno@gmail.com>
2015-12-30 00:21:40 +0100

    update add json server

--- conFusion/gulpfile.js
+++ conFusion/gulpfile.js
@@ -31,17 +31,14 @@ gulp.task('default', ['clean'], function() {
     gulp.start('usemin', 'imagemin','copyfonts');
 }); 
 
-
 gulp.task('usemin',['jshint'], function () {
-  return gulp.src('./app/menu.html')
+  return gulp.src('./app/**/*.html')
       .pipe(usemin({
         css:[minifycss(),rev()],
         js: [ngannotate(),uglify(),rev()]
       }))
       .pipe(gulp.dest('dist/'));
 });
-
-
 
 // Images
 gulp.task('imagemin', function() {
@@ -76,12 +73,14 @@ gulp.task('browser-sync', ['default'], function () {
       'dist/**/*'
    ];
 
-   browserSync.init(files, {
+  browserSync.init(files, {
       server: {
          baseDir: "dist",
-         index: "menu.html"
+         index: "index.html"
       }
    });
-        // Watch any files in dist/, reload on change
+    
+    
+  // Watch any files in dist/, reload on change
   gulp.watch(['dist/**']).on('change', browserSync.reload);
     });
